cfc17b9bd5224439ae475559265e6296bee1f666
tomkotlar <t.kotlar@yahoo.com>
2020-10-08 15:33:21 +0100

    Move Http req to serprate file

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,19 @@
+import { Observable } from "rxjs";
 
-
+export function createHttpObservable(url:string) {
+    return new Observable(observer => {
+       fetch('/api/cousrse')
+       .then(response => {
+         return response.json()
+       })
+       .then(body => {
+        //  observable pattern, after complete its sotp the value stream
+         observer.next(body)
+         observer.complete()
+       })
+      //  catching err
+       .catch(err => {
+         observer.error(err)
+       })
+     })
+   }
