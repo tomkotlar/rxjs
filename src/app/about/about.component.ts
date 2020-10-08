a42e4933b1291dafba61b00f740600126b1e2623
tomkotlar <t.kotlar@yahoo.com>
2020-10-08 15:34:17 +0100

    Lesson 10:Refactor the call with map Operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,58 +1,26 @@
 import { ConstantPool } from "@angular/compiler";
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { fromEvent, interval, noop, Observable, timer } from "rxjs";
-
+import { map } from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
 @Component({
   selector: "about",
   templateUrl: "./about.component.html",
   styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-  constructor() {}
+  constructor() { }
 
   ngOnInit() {
-    // creating own Observable pattern 
-   const http$ =  new Observable(observer => {
-     fetch('/api/cousrse')
-     .then(response => {
-       return response.json()
-     })
-     .then(body => {
-      //  observable pattern, after complete its sotp the value stream
-       observer.next(body)
-       observer.complete()
-     })
-    //  catching err
-     .catch(err => {
-       observer.error(err)
-     })
-   })
-   
-  //  sunbscribing to value stream
-   http$.subscribe(
-     course => console.log(course),
-    // noop stands for no operation 
-    noop,
-     () => console.log("compleated")
-   )
-  
+    const http$ = createHttpObservable("/api/course");
+
+    const courses$ = http$.pipe(map((res) => Object.values(res["payload"])));
+
+    courses$.subscribe(
+      (course) => console.log(course),
+      // noop stands for no operation
+      noop,
+      () => console.log("compleated")
+    );
   }
 }
-
-
-
-  // // Observable blueprint for stream
-
-    // // const interval$ = interval(1000)
-    // const interval$ = timer(3000, 1000);
-
-    // const sub = interval$.subscribe((val) => console.log("strea, => 1" + val));
-    // // unsubscribing from stream of values.
-    // setTimeout(() => sub.unsubscribe(), 5000)
-
-    // const click$ = fromEvent(document, "click");
-    // click$.subscribe(
-    //   (evt) => console.log(evt),
-    //   (err) => console.log(err), 
-    //   () => console.log("completed")
-    // );
