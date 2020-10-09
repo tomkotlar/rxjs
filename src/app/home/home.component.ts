6b7ba5e515b571f233b8e191f15ad811ca4bbb24
tomkotlar <t.kotlar@yahoo.com>
2020-10-09 12:11:20 +0100

    Lesson 13: Sharing HTTP Responses with the shareReplay Operator

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer, noop} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { Course } from "../model/course";
+import { interval, Observable, of, timer, noop } from 'rxjs';
+import { catchError, delayWhen, map, retryWhen, shareReplay, tap } from 'rxjs/operators';
 import { createHttpObservable } from "../common/util";
 
 @Component({
@@ -10,39 +10,35 @@ import { createHttpObservable } from "../common/util";
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-// blueprint for stream of data
+    // blueprint for stream of data
     beginnerCourses$: Observable<Course[]>
     advancedCourses$: Observable<Course[]>
 
-    constructor() {
-
-    }
 
     ngOnInit() {
 
         const http$ = createHttpObservable("/api/course");
 
-        const courses$: Observable<Course[]> = http$.pipe(map(res => Object.values(res["payload"])));
-   
+        const courses$: Observable<Course[]>  = http$
+            .pipe(
+                // Tap is used to produce side effects in our observable chain.
+                // So whenever we want to update something outside of the observable chain; for example updating
+                // a variable at the level of the component or a logging statement.
+                tap(() => console.log("HTTP request started")), 
+                map(res => Object.values(res["payload"])),
+                shareReplay()
+                // passing the data from res to each each /new subscription/stream. prenvent multiple http call 
+            );
+
 
         this.beginnerCourses$ = courses$.pipe(
             map(couses => couses
                 .filter(course => course.category == 'BEGINNER'))
         )
-        this.advancedCourses$ = courses$ .pipe(
-            map(couses => couses.filter(course => course.category == 'ADVANCED'))
+        this.advancedCourses$ = courses$.pipe(
+            map(couses => couses
+                .filter(course => course.category == 'ADVANCED'))
         )
-
-
-
-        courses$.subscribe(
-          courses => {
-           
-        }, 
-        // noop stands for no operation
-        noop,
-          () => console.log("compleated")
-        );
 
     }
 
